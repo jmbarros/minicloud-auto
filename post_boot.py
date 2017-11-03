77966f36c709258af4b60d9a3694c5d6b6e63930
JMBarros <jmbarros@users.noreply.github.com>
2017-11-03 12:17:20 -0200

    fix post_boot.py

--- post_boot.py
+++ post_boot.py
@@ -14,4 +14,7 @@ def python ( py ):
   return;
 
 ####################################################
-python("/root/minicloud-auto/to_execute.py")
+#python("/root/minicloud-auto/to_execute.py")
+python("/root/minicloud-auto/update_inventory.py")
+python("/root/minicloud-auto/install.py")
+cmd("/usr/bin/systemctl disable post_boot.service")
